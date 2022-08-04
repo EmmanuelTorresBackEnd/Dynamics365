5f85d145f9e02f027f4ede2162203e325b012a70
Emmanuel <emmanueltorres21@live.com>
2022-08-04 15:47:47 -0300

    Dynamics365

---
+++ DynacoopConsole1/ConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynacoopConsole1
+{
+    internal class ConnectionFactory
+    {
+        public static IOrganizationService GetService()
+        {
+            string connectionString =
+             "AuthType=OAuth;" +
+             "Username=Emmanuel@Dynacoops.onmicrosoft.com; " +
+             "Password=Caps1367#;" +
+             "Url=https://org8993d19f.crm2.dynamics.com/;" +
+             "AppId=95d0d068-9056-43de-b236-777e997d0892;" +
+             "RedirectUri=app://9896d91c-3ad3-4762-918f-3024bebc5d1a;";
+
+
+            CrmServiceClient crmServiceClient = new CrmServiceClient(connectionString);
+            return crmServiceClient.OrganizationWebProxyClient;
+
+        }
+
+    }
+
+}
---
+++ DynacoopConsole1/Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DynacoopConsole1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            IOrganizationService service = ConnectionFactory.GetService();
+
+
+            //CONTA
+            Entity conta = new Entity("account");
+            Console.WriteLine("Por Favor informe o nome da conta");
+            conta["name"] = Console.ReadLine();
+
+            Console.WriteLine($"Digite seu Telefone ex:(DDD)99999-9999");
+            conta["telephone1"] = Console.ReadLine();
+
+            Console.WriteLine($"Digite seu E-mail");
+            conta["emailaddress1"] = Console.ReadLine();
+
+            Console.WriteLine($"Possui Fax? se não Aperte ENTER!");
+            conta["fax"] = Console.ReadLine();
+
+            Console.WriteLine($"Digite seu WebSite");
+            conta["websiteurl"] = Console.ReadLine();
+
+            Guid accountId = service.Create(conta);
+
+            Console.WriteLine($"Caso queira criar um Contato prossiga com o cadastro, caso não queira tecle ENTER em todos os campos!");
+
+            //CONTATO
+            Entity contato = new Entity("contact");
+            Console.WriteLine($"Digite o primeiro nome");
+
+            contato["firstname"] = Console.ReadLine();
+            Console.WriteLine($"Digite o sobrenome");
+
+            contato["lastname"] = Console.ReadLine();
+            Console.WriteLine($"Digite sobrenome2");
+
+            contato["jobtitle"] = Console.ReadLine();
+            Console.WriteLine($"Digite sua Profissão");
+
+            contato["accountid"] = new EntityReference("account" , accountId);
+
+            service.Create(contato);
+
+
+            Console.WriteLine($"Obrigado por utilizar nosso sistema!!");
+
+            Console.Write("Finalizando");
+            Thread.Sleep(1000);
+            for (var i = 0; i < 5; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(300);
+            }
+
+        }
+
+    }
+
+}
+
+    
+
+
+
+
+
+            
+
+ 
